95bcdf3951bb968788fe05b2532a1d2aba27ae38
Asche <christopher.asche@sap.com>
2016-02-03 15:22:26 +0100

    Sets up npm, mocha, and githooks for tests

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+  return gulp.src(['tests/**.js'], {
+    read: false
+  }).pipe(mocha({
+    reporter: 'nyan'
+  }));
+});
