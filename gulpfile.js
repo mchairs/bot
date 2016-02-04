04b343e221cbb59a58251bf8d329d6a03ba9e5d5
Asche <chris.asche@gmail.com>
2016-02-04 09:12:31 +0100

    Adds jscs and eslint + config files for both

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
+var eslint = require('gulp-eslint');
+var jscs = require('gulp-jscs');
 
 gulp.task('default', function() {
   nodemon({
@@ -18,4 +20,17 @@ gulp.task('test', function() {
   }).pipe(mocha({
     reporter: 'nyan'
   }));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['bot/**/*.js', 'tests/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('style', function() {
+  return gulp.src(['bot/**/*.js', 'tests/**/*.js'])
+    .pipe(jscs())
+    .pipe(jscs.reporter());
 });
