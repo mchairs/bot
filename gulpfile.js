d879d69ecd2c078b37b69fcfd6a2a3dfc6bde33e
Asche <chris.asche@gmail.com>
2016-02-04 09:05:36 +0100

    Sets up nodemon for server.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default', function() {
+  nodemon({
+    script: 'server.js',
+    ext: 'js html',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  });
+});
 
 gulp.task('test', function() {
   return gulp.src(['tests/**.js'], {
