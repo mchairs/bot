824b32d1be3a8715f12c1e56fb2114cd6b263126
Asche <chris.asche@gmail.com>
2016-02-13 13:22:16 -0400

    server, don't do anything just yet

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,1 @@
 'use strict';
-
-if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
-  console.log('Sorry dude, you need to specify a clientId, clientSecret and port before you can rock your bot');
-  process.exit(1);
-}
-
-function test() {
-}
