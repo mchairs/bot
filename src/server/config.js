7037b6fcecbd1d5b9510e89201d1a80b9f8f5a3f
Asche <chris.asche@gmail.com>
2016-02-13 17:53:22 -0400

    config must specify clientId, clientSecret and a port

---
+++ src/server/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default {
+  ok: () => process.env.cliendId && process.env.clientSecret && process.env.port
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,7 @@
 'use strict';
+
+import config from './config.js';
+
+if (!config.ok()) {
+  console.log('Sorry dude, to launch your bot you need a clientId, clientSecret and port');
+}
