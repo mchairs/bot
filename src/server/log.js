da27db1837ae70e8320d977b008aaf06db94eae4
Asche <chris.asche@gmail.com>
2016-02-13 19:08:15 -0400

    Removes the comment, looks like we can use winston for botkit so we're keeping it for now.

--- src/server/log.js
+++ src/server/log.js
@@ -1,7 +1,6 @@
 import winston from 'winston';
 import moment from 'moment';
 
-// just in case we ever want to have more complicated logging
 export default new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
