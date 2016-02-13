874afe9079efab21027c3e1e418059baca966884
Asche <chris.asche@gmail.com>
2016-02-13 12:33:46 -0400

    Adds compile task to transpile bot/**/*.js && server.js to ./dist. Changes default task to compile and watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,27 @@ var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
 var eslint = require('gulp-eslint');
 var jscs = require('gulp-jscs');
-var babel = require('babel-core/register')
+var babelC = require('babel-core/register');
+var babel = require('gulp-babel');
 
-gulp.task('default', () => {
+gulp.task('compile', () => {
+  gulp.src(['bot/**/*.js', 'server.js'])
+      .pipe(babel())
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('start', ['compile'], () => {
   nodemon({
-    script: 'server.js',
-    ext: 'js html',
+    script: 'dist/server.js',
+    ext: 'js',
+    tasks: ['compile'],
     env: {
       'NODE_ENV': 'development'
     }
   });
 });
+
+gulp.task('default', ['start']);
 
 gulp.task('test', () => {
   return gulp.src(['tests/**/*.js'], {
@@ -21,7 +31,7 @@ gulp.task('test', () => {
   }).pipe(mocha({
     reporter: 'nyan',
     compilers: {
-      js: babel
+      js: babelC
     }
   }));
 });
