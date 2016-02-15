5f40a84097a4ddfea3f495a79acb763233500ac7
Asche <chris.asche@gmail.com>
2016-02-15 15:13:11 -0400

    stubs out user service

--- src/server/bot/services/user.service.js
+++ src/server/bot/services/user.service.js
@@ -3,15 +3,15 @@
 let User = require('../documents/user.doc.js');
 
 module.exports = {
-    get: () => {
+    get: (userId, done) => {
         User.find({userId: userId}, done);
     },
 
-    save: () => {
-        
+    save: (userData, done) => {
+        new User(userData).save(done);
     },
 
-    all: () => {
-
+    all: (done) => {
+        User.find({}, done);
     }
 };
