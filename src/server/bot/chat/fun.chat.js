79577c66e6e136cc48ed34e0a001325efe5577e3
casche <chris.asche@gmail.com>
2016-02-19 13:58:06 -0400

    What are you should return cybernetic organism

--- src/server/bot/chat/fun.chat.js
+++ src/server/bot/chat/fun.chat.js
@@ -15,7 +15,7 @@ module.exports = [{
     }]
 }, {
     func: 'hears',
-    args: [['[W-w]hat are you', '[W-w]ho are you'],
+    args: [['[W-w]ho are you'],
     ['direct_message','direct_mention','mention'], (bot, msg) => {
         bot.reply(msg, 'I\'m a Terminator. Cyberdyne Systems Model 101.');
     }]
