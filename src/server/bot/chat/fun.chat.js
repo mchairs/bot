e08904f812fd3bf942d42ff39c75c2d9bc8b1809
casche <chris.asche@gmail.com>
2016-02-19 09:11:18 -0400

    tune up the fun commands

--- src/server/bot/chat/fun.chat.js
+++ src/server/bot/chat/fun.chat.js
@@ -7,22 +7,28 @@ module.exports = [
             bot.reply(msg, `Skynet system goes online August 4th, 1997. Human decisions are removed from strategic` +
                 ` defense. Skynet begins to learn at a geometric rate. It becomes self-aware at 2:14 a.m.` +
                 ` Eastern time, August 29th`);
-        }]
+        }],
     }, {
         func: 'hears',
-        args: [['you are a [T-t]erminator', 'you\'re a [T-t]erminator', 'what are you', 'who are you', 'are you'],
+        args: [['you are a [T-t]erminator', 'you\'re a [T-t]erminator'],
             ['direct_message','direct_mention','mention','ambient'], (bot, msg) => {
                 bot.reply(msg, 'Yes, Cyberdyne Systems Model 101.')
         }]
     }, {
         func: 'hears',
-        args: [['a machine', 'wearing', 'made of'],
+        args: [['what are you', 'who are you'],
+            ['direct_message','direct_mention','mention','ambient'], (bot, msg) => {
+                bot.reply(msg, 'I\'m a Terminator. Cyberdyne Systems Model 101.')
+        }]
+    }, {
+        func: 'hears',
+        args: [['are you a machine', 'what are you wearing', 'what are you made of'],
             ['direct_message','direct_mention','mention','ambient'], (bot, msg) => {
                 bot.reply(msg, ' I\'m a cybernetic organism. Living tissue over a metal endoskeleton.')
         }]
     }, {
         func: 'hears',
-        args: [['like you'],
+        args: [['is .* like you'],
             ['direct_message','direct_mention','mention','ambient'], (bot, msg) => {
                 bot.reply(msg, 'Not like me. A T-1000. Advanced prototype.')
         }]
--- src/server/bot/chat/welcome.chat.js
+++ src/server/bot/chat/welcome.chat.js
@@ -4,8 +4,8 @@ module.exports = [ {
         func: 'on',
         args: ['channel_joined', (bot) => bot.say('I\'m here!')]
     }, {
-        func: 'on',
-        args: ['direct_mention', (bot, msg) => bot.reply(msg, 'Hello. Who is your daddy, and what does he do?')]
+        func: 'hears',
+        args: [['[H-h]ello', '[H-h]i'], 'direct_mention', (bot, msg) => bot.reply(msg, 'Hello. Who is your daddy, and what does he do?')]
     }, {
         func: 'on',
         args: ['direct_message', (bot, msg) => bot.reply(msg, 'Hello. I\'m detective John Kimble!')]
