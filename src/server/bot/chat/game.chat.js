d5a91ef23455bc2ddc3fd90056523b397a795ba4
casche <chris.asche@gmail.com>
2016-02-19 14:30:11 -0400

    match is an array not a function

--- src/server/bot/chat/game.chat.js
+++ src/server/bot/chat/game.chat.js
@@ -4,20 +4,20 @@ module.exports = [
     {
         func: 'hears',
         args: [['start (.*) (\\d)'], ['direct_mention'], (bot, msg) => {
-            var time = msg.match(1);
-            var numchairs = msg.match(2);
+            var time = msg.match[1];
+            var numchairs = msg.match[2];
             bot.reply(msg, `Ok, let's start a game at ${time} with ${numchairs} chairs`);
         }]
     }, {
         func: 'hears',
         args: [['sit (\\d)'], ['direct_message'], (bot, msg) => {
-            var c = msg.match(1);
+            var c = msg.match[1];
             bot.reply(msg, `Ok, you can sit on ${c}`);
         }]
     }, {
         func: 'hears',
         args: [['force (\d)'], ['direct_message'], (bot, msg) => {
-            var c = msg.match(1);
+            var c = msg.match[1];
             bot.reply(msg, `Ok, you can force on ${c}`);
         }]
     }
