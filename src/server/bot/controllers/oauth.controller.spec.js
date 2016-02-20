712fc3c426c1148633810584de3f57ef6f2eb54b
casche <chris.asche@gmail.com>
2016-02-19 22:06:53 -0400

    Tests the oAuth controller to verify it sends the proper response code or success

--- src/server/bot/controllers/oauth.controller.spec.js
+++ src/server/bot/controllers/oauth.controller.spec.js
@@ -1,9 +1,26 @@
 'use strict';
 
 const oAuth = require('./oauth.controller.js');
+const sinon = require('sinon');
+const assert = require('assert');
 
 describe('oAuth Controller', () => {
-    it('should do stuff', () => {
+    it('should send a 500 in the response on a failure', () => {
+        oAuth({}, {}, {
+            status: (errorCode) => {
+                assert.equal(errorCode, 500);
+                return {
+                    send: sinon.spy()
+                };
+            }
+        });
+    });
 
+    it('should respond with success without an error', () => {
+        var res = {
+            send: sinon.spy()
+        };
+        oAuth(undefined, {}, res);
+        assert(res.send.calledOnce);
     });
 });
