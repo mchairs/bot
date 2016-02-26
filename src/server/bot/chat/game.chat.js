1cdba1192ebad55bb454fff5d8bebf6ad7edf45a
casche <chris.asche@gmail.com>
2016-02-26 14:31:48 -0400

    beautify game.chat.js

--- src/server/bot/chat/game.chat.js
+++ src/server/bot/chat/game.chat.js
@@ -3,10 +3,11 @@
 const GameService = require('../../game/services/game.service.js');
 const Extract = require('./extract.js');
 
-module.exports = [
-{
-        func: 'hears',
-        args: [['start (.*) (\\d)'], ['direct_mention'], (bot, msg) => {
+module.exports = [{
+    func: 'hears',
+    args: [
+        ['start (.*) (\\d)'],
+        ['direct_mention'], (bot, msg) => {
             var time = msg.match[1];
             var chairs = msg.match[2];
             if (time && chairs) {
@@ -14,18 +15,24 @@ module.exports = [
                     if (!err) bot.reply(msg, `Ok, let's start a game at ${game.date} with ${game.chairs} chairs`);
                 });
             }
-        }]
-    }, {
-        func: 'hears',
-        args: [['sit (\\d)'], ['direct_message'], (bot, msg) => {
+        }
+    ]
+}, {
+    func: 'hears',
+    args: [
+        ['sit (\\d)'],
+        ['direct_message'], (bot, msg) => {
             var c = msg.match[1];
             bot.reply(msg, `Ok, you can sit on ${c}`);
-        }]
-    }, {
-        func: 'hears',
-        args: [['force (\\d)'], ['direct_message'], (bot, msg) => {
+        }
+    ]
+}, {
+    func: 'hears',
+    args: [
+        ['force (\\d)'],
+        ['direct_message'], (bot, msg) => {
             var c = msg.match[1];
             bot.reply(msg, `Ok, you can force on ${c}`);
-        }]
-    }
-];
+        }
+    ]
+}];
